b8aabb7863d203487ed7dca2b6190c78f17439ba
Eric Mountain <eric.mountain@gmail.com>
2018-11-10 22:54:07 +0100

    Start refactoring: writeMetadata function, hashes + only write metadata on successful backup

--- bin/backup.sh
+++ bin/backup.sh
@@ -29,25 +29,48 @@ EOF
     false
 }
 
+writeMetadata() {
+    local x=$1
+    local -n configRef=$2
+    echo -- $x: saving metadata
+    sudo /bin/bash -c  "cat - > __metadata/${x}_${configRef[uuid]}.btrfs-backups.metadata" <<EOF
+# Backup of ${x} for device pair ${configRef[uuid]}
+timestamp=$(date -u "+%Y-%m-%d_%H:%M:%S_%Z")
+
+source_hostname=$(hostname)
+source_mountpath=${configRef[source_active_path]}
+source_volume_name=${x}
+source_uuid_label=${configRef[source_uuid_label]}
+source_uuid_devid=${configRef[source_uuid_devid]}
+source_uuid_dir=${configRef[source_uuid_dir]}
+source_uuid=${configRef[source_uuid]}
+
+target_uuid_label=${configRef[target_uuid_label]}
+target_uuid_devid=${configRef[target_uuid_devid]}
+target_uuid_dir=${configRef[target_uuid_dir]}
+target_uuid=${configRef[target_uuid]}
+EOF
+}
+
+## Main ##############################################################
+
 getopt=$(getopt -n $0 -o h -l source:,target:,target-shell:,help -- "$@")
 eval set -- "$getopt"
 
-declare source_root=""
-declare target_root=""
-declare target_shell=""
+declare -A config=( [source_root]="" [target_root]="" [target_shell]="" )
 
 while true ; do
     case "$1" in
         --source)
-            source_root=$2
+            config[source_root]=$2
             shift 2
             ;;
         --target)
-            target_root=$2
+            config[target_root]=$2
             shift 2
             ;;
         --target-shell)
-            target_shell="$2"
+            config[target_shell]="$2"
             shift 2
             ;;
         --help|h)
@@ -64,51 +87,55 @@ while true ; do
     esac
 done
 
-[ -z ${source_root} ] && echo Need --source. && false
-[ -z ${target_root} ] && echo Need --target. && false
+[ -z ${config[source_root]} ] && echo Need --source. && false
+[ -z ${config[target_root]} ] && echo Need --target. && false
 
 ionice -c 3 -p $$
 renice -n 20 $$ > /dev/null 2>&1
 
-source_active_dir=__active
-source_backup_dir=__backups
-source_active_path="${source_root}/${source_active_dir}"
-source_backup_path="${source_root}/${source_backup_dir}"
+config[source_active_dir]=__active
+config[source_backup_dir]=__backups
+config[source_active_path]="${config[source_root]}/${config[source_active_dir]}"
+config[source_backup_path]="${config[source_root]}/${config[source_backup_dir]}"
 
-target_active_dir=__active
-target_active_path="${target_root}/${target_active_dir}"
+config[target_active_dir]=__active
+config[target_active_path]="${config[target_root]}/${config[target_active_dir]}"
 
-[ -d "${source_backup_path}" ] || sudo mkdir -p "${source_backup_path}"
-${target_shell} [ -d "${target_active_path}" ] || ${target_shell} sudo mkdir -p "${target_active_path}"
+[ -d "${config[source_backup_path]}" ] || sudo mkdir -p "${config[source_backup_path]}"
+${config[target_shell]} [ -d "${config[target_active_path]}" ] || ${config[target_shell]} sudo mkdir -p "${config[target_active_path]}"
 
 # Whether sending locally or over ssh, wherever the destination device
 # is plugged, we will recognise it and update the last backup if
 # present, or send a full stream. Including the active directory in the
 # ID allows for multiple such directories on the destination.
-target_uuid_label=$(${target_shell} sudo btrfs fi show "${target_root}" | grep uuid | sed -e "s/^.*'\(.*\)'.*\$/\1/")
-target_uuid_devid=$(${target_shell} sudo btrfs fi show "${target_root}" | grep uuid | sed -E -e "s/^.*uuid: ([^ ]+).*\$/\1/")
-target_uuid_dir="${target_active_dir}"
-target_uuid=$(echo $target_uuid_label $target_uuid_devid $target_uuid_dir | sha1sum | awk '{print $1}')
+config[target_uuid_label]=$(${config[target_shell]} sudo btrfs fi show "${config[target_root]}" | grep uuid | sed -e "s/^.*'\(.*\)'.*\$/\1/")
+config[target_uuid_devid]=$(${config[target_shell]} sudo btrfs fi show "${config[target_root]}" | grep uuid | sed -E -e "s/^.*uuid: ([^ ]+).*\$/\1/")
+config[target_uuid_dir]="${config[target_active_dir]}"
+config[target_uuid]=$(echo ${config[target_uuid_label]} ${config[target_uuid_devid]} ${config[target_uuid_dir]} | sha1sum | awk '{print $1}')
 
 # Avoid conflicts with identical source names coming from other pools
 # when backing up
-source_uuid_label=$(sudo btrfs fi show "${source_root}" | grep uuid | sed -e "s/^.*'\(.*\)'.*\$/\1/")
-source_uuid_devid=$(sudo btrfs fi show "${source_root}" | grep uuid | sed -E -e "s/^.*uuid: ([^ ]+).*\$/\1/")
-source_uuid_dir="${source_active_dir}"
-source_uuid=$(echo $source_uuid_label $source_uuid_devid $source_uuid_dir | sha1sum | awk '{print $1}')
+config[source_uuid_label]=$(sudo btrfs fi show "${config[source_root]}" | grep uuid | sed -e "s/^.*'\(.*\)'.*\$/\1/")
+config[source_uuid_devid]=$(sudo btrfs fi show "${config[source_root]}" | grep uuid | sed -E -e "s/^.*uuid: ([^ ]+).*\$/\1/")
+config[source_uuid_dir]="${config[source_active_dir]}"
+config[source_uuid]=$(echo ${config[source_uuid_label]} ${config[source_uuid_devid]} ${config[source_uuid_dir]} | sha1sum | awk '{print $1}')
 
-uuid=${source_uuid}_${target_uuid}
+config[uuid]=${config[source_uuid]}_${config[target_uuid]}
 
-cd "${source_active_path}"
-[ -d "${source_active_path}/__metadata" ] || sudo btrfs subvolume create "${source_active_path}/__metadata"
-let doMetadata=0 || true
-let errorsOccurred=0 || true
+cd "${config[source_active_path]}"
+[ -d "${config[source_active_path]}/__metadata" ] || sudo btrfs subvolume create "${config[source_active_path]}/__metadata"
+
+declare -A status=( [doMetadata]=0 [errorsOccurred]=0 )
+declare -A bkp
 for x in * __metadata ; do
+    bkp=([hasError]=0)
+
     [ -d "$x" ] || continue
 
     # Ensure __metadata is backed up last
-    if [ $doMetadata -eq 0 -a "${x}" == "__metadata" ]  ; then
-        let doMetadata=1
+    if [ ${status[doMetadata]} -eq 0 -a "${x}" == "__metadata" ]  ; then
+        status[doMetadata]=1
+        writeMetadata $x config
         continue
     fi
 
@@ -117,67 +144,57 @@ for x in * __metadata ; do
         continue
     fi
 
-    echo -- $x: saving metadata
-    sudo /bin/bash -c  "cat - > __metadata/${x}_${uuid}.btrfs-backups.metadata" <<EOF
-# Backup of ${x} for device pair ${uuid}
-timestamp=$(date -u "+%Y-%m-%d_%H:%M:%S_%Z")
-
-source_hostname=$(hostname)
-source_mountpath=${source_active_path}
-source_volume_name=${x}
-source_uuid_label=$source_uuid_label
-source_uuid_devid=$source_uuid_devid
-source_uuid_dir=$source_uuid_dir
-source_uuid=$source_uuid
-
-target_uuid_label=$target_uuid_label
-target_uuid_devid=$target_uuid_devid
-target_uuid_dir=$target_uuid_dir
-target_uuid=$target_uuid
-EOF
-
     echo -- $x: starting backup
 
     # Snapshot active directory to serve as reference next time round
-    source_backup="${source_backup_path}/${x}_${uuid}"
-    source_backup_new="${source_backup_path}/.${x}_${uuid}_new"
-    parent_opt=""
-    latest_backup_path=""
-    if [ -d "${source_backup}" ] ; then
-        parent_opt="-p"
-        latest_backup_path="${source_backup}"
+    bkp[source_backup]="${config[source_backup_path]}/${x}_${config[uuid]}"
+    bkp[source_backup_new]="${config[source_backup_path]}/.${x}_${config[uuid]}_new"
+    bkp[parent_opt]=""
+    bkp[latest_backup_path]=""
+    if [ -d "${bkp[source_backup]}" ] ; then
+        bkp[parent_opt]="-p"
+        bkp[latest_backup_path]="${bkp[source_backup]}"
     fi
 
-    [ -d "${source_backup_new}" ] && sudo btrfs subvolume delete "${source_backup_new}"
+    [ -d "${bkp[source_backup_new]}" ] && sudo btrfs subvolume delete "${bkp[source_backup_new]}"
 
-    if ! sudo btrfs subvolume snapshot -r "$x" "${source_backup_new}" ; then
-        sudo btrfs subvolume delete "${source_backup_new}" || true
-        echo -- $x: error creating snapshot
-        let errorsOccurred=1
+    if ! sudo btrfs subvolume snapshot -r "$x" "${bkp[source_backup_new]}" ; then
+        sudo btrfs subvolume delete "${bkp[source_backup_new]}" || true
+        echo -- $x: error creating snapshot "${bkp[source_backup_new]}"
+        status[errorsOccurred]=1
+        bkp[hasError]=1
     fi
 
-    destination_active="${target_active_path}/${x}_${uuid}"
-    destination_active_new="${target_active_path}/.${x}_${uuid}_new"
-    ${target_shell} [ -d "${destination_active_new}" ] && ${target_shell} sudo btrfs subvolume delete "${destination_active_new}"
+    bkp[destination_active]="${config[target_active_path]}/${x}_${config[uuid]}"
+    bkp[destination_active_new]="${config[target_active_path]}/.${x}_${config[uuid]}_new"
+    ${config[target_shell]} [ -d "${bkp[destination_active_new]}" ] && ${config[target_shell]} sudo btrfs subvolume delete "${bkp[destination_active_new]}"
 
     # Send snapshot to target.
-    if sudo btrfs send ${parent_opt} ${latest_backup_path} "${source_backup_new}" | \
-            ${target_shell} sudo btrfs receive "${target_active_path}" ; then
+    if sudo btrfs send ${bkp[parent_opt]} ${bkp[latest_backup_path]} "${bkp[source_backup_new]}" | \
+            ${config[target_shell]} sudo btrfs receive "${config[target_active_path]}" ; then
 
-        [ -d "${source_backup}" ] && sudo btrfs subvolume delete "${source_backup}"
-        sudo mv "${source_backup_new}" "${source_backup}"
+        [ -d "${bkp[source_backup]}" ] && sudo btrfs subvolume delete "${bkp[source_backup]}"
+        sudo mv "${bkp[source_backup_new]}" "${bkp[source_backup]}"
 
-        ${target_shell} [ -d "${destination_active}" ] && ${target_shell} sudo btrfs subvolume delete "${destination_active}"
-        ${target_shell} sudo mv "${destination_active_new}" "${destination_active}"
+        ${config[target_shell]} [ -d "${bkp[destination_active]}" ] && ${config[target_shell]} sudo btrfs subvolume delete "${bkp[destination_active]}"
+        ${config[target_shell]} sudo mv "${bkp[destination_active_new]}" "${bkp[destination_active]}"
 
         echo -- $x: backed up
     else
-        sudo btrfs subvolume delete "${source_backup_new}" || true
-        ${target_shell} sudo btrfs subvolume delete "${destination_active_new}" || true
+        sudo btrfs subvolume delete "${bkp[source_backup_new]}" || true
+        ${config[target_shell]} sudo btrfs subvolume delete "${bkp[destination_active_new]}" || true
 
         echo -- $x: error sending snapshot
-        let errorsOccurred=1
+        status[errorsOccurred]=1
+        bkp[hasError]=1
     fi
-done
 
-exit $errorsOccurred
+    # Only update metadata after a successful backup, except when backing up 
+    # metadata itself (in which case, the metadata needs to be updated before
+    # backing up else the backup target will hold stale data)
+    if [ "${x}" != "__metadata" -a ${bkp[hasError]} -eq 0 ] ; then
+        writeMetadata $x config
+    fi
+done    
+
+exit ${status[errorsOccurred]}
