ee10107a0425abed89d7b46965d6e57ad68bfbd3
Eric Mountain <eric.mountain@gmail.com>
2018-11-18 21:35:17 +0100

    removes usage from common.sh

--- bin/common.sh
+++ bin/common.sh
@@ -7,15 +7,3 @@ catch() {
 }
 
 trap 'catch "Error caught (line $LINENO, exit code $?)"' ERR
-
-usage() {
-    cat - <<EOF
-Usage: $0 [args]
-
---source       Source btrfs pool
---target       Target btrfs pool
-EOF
-
-    false
-}
-
